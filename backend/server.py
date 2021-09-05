85f1a6b9601c5c40fada4bddb55300dad3078d5d
ivanvlizarazo <ivanlizarazo1305@gmail.com>
2021-09-04 23:53:56 -0500

    first commit in production

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,12 @@
 from flask import Flask,request
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="../build",static_url_path="/")
 CORS(app)
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
 
 @app.route('/requested_amount', methods=["POST"])
 def requested_amount():
